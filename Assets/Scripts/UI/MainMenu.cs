ef980f66426f6a5212a32c5067f977d970390006
MavrichevAndrey <Materials46@gmail.com>
2020-12-14 18:06:09 +0300

    + ui toolkit asset

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        private VisualElement root;
+
+        public void Start()
+        {
+            root = GetComponent<UIDocument>().rootVisualElement;
+
+            var startButton = root.Q<Button>("start");
+            var optionsButton = root.Q<Button>("options");
+            var exitButton = root.Q<Button>("exit");
+
+            startButton.text = "START";
+
+            //root.Q<Button>("exit").RegisterCallback<MouseDownEvent>();
+
+
+
+            startButton.clicked += StartButtonPressed;
+            optionsButton.clicked += StartButtonPressed;
+            exitButton.clicked += ExitButtonPressed;
+        }
+
+        private void StartButtonPressed()
+        {
+            SceneManager.LoadSceneAsync("Level 1");
+        }
+
+        private void ExitButtonPressed()
+        {
+            Application.Quit();
+        }
+    }
+}
